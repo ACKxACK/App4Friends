96fc4f58897296dc72903b849338d0b91ddd2d64
Ali Can Kayaaslan <ackayaaslan@gmail.com>
2023-03-30 14:08:12 +0300

    Initial Commit

---
+++ App4Friends/App4FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  App4FriendsApp.swift
+//  App4Friends
+//
+//  Created by Ali Can Kayaaslan on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct App4FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
